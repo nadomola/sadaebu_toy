2a07a7670773e3474078c3f1fcdc842509ee98f8
nadomola <siyun778@gmail.com>
2024-01-26 22:27:37 +0900

    first commit

---
+++ sadaeboo_toy/src/test/java/com/example/sadaeboo_toy/SadaebooToyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sadaeboo_toy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadaebooToyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
